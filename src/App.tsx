c91af12dccfb2f1940e89755441b696b2bdc07f4
Gordon Chai <chaigordon75@gmail.com>
2025-01-07 18:09:35 +0800

    Updated footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <NavBar />
+      <div className="flex flex-col min-h-screen">
+        {" "}
+        {/* This ensures the container takes up at least the full height of the screen */}
+        <NavBar />
+        <main className="flex-grow">
+          {" "}
+          {/* This will grow to take up any remaining space */}
+          {/* Your page content goes here */}
+        </main>
+        <Footer />
+      </div>
     </>
   );
 }
