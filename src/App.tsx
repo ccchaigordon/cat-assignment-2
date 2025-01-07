468ec149095f17235972026b967ded51f2442158
Gordon Chai <chaigordon75@gmail.com>
2025-01-07 23:34:41 +0800

    Updated App

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import Attractions from "./components/Attractions";
 
 function App() {
   return (
     <>
       <div className="flex flex-col min-h-screen">
         {" "}
-        {/* This ensures the container takes up at least the full height of the screen */}
         <NavBar />
         <main className="flex-grow">
           {" "}
-          {/* This will grow to take up any remaining space */}
-          {/* Your page content goes here */}
+          <Attractions />
         </main>
         <Footer />
       </div>
