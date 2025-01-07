d5adfa38c4c4a56f3f6a37267bdf1d4d81c334ba
Gordon Chai <chaigordon75@gmail.com>
2025-01-08 01:57:57 +0800

    Added attractions #1

---
+++ src/components/Attractions.tsx
@@ -0,0 +1,19 @@
+function Attractions() {
+  return (
+    <>
+      <div className="slideLeft-element my-20 block">
+        <div className="absolute z-0 w-5/6 md:w-4/6 h-[20vw] left-0 bg-transparent rounded-[0px_222px_222px_0px] border-[2px] sm:border-[3px] border-l-0 sm:border-l-0 border-solid border-[#1c42ca]" />
+        <div className="flex flex-col h-80 w-full pt-[6vw] pl-[5vw] md:pl-[4vw]">
+          <p className="[z-10 font-family:'Poppins-Regular',Helvetica] font-normal text-black text-[2.5vw] md:text-[1.5vw] text-justify tracking-[0] leading-[normal]">
+            Must-Visit Gems You Can't Miss in Penang
+          </p>
+          <div className="[font-family:'Poppins',Helvetica] font-bold text-black text-[5vw] md:text-[4vw] text-justify tracking-[0] leading-[normal]">
+            Explore Penang Attractions
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Attractions;
