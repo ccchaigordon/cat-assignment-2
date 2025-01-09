cce98ef5eb43d7291e1c0bfc27e1adab1b8fe476
Gordon Chai <chaigordon75@gmail.com>
2025-01-09 23:33:48 +0800

    Added custom animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,18 @@ export default {
       screens: {
         'xs': '400px',
         'lg2': '960px',
-      }
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-10px)' },
+        },
+      },
+      animation: {
+        float: 'float 3s ease-in-out infinite',
+        "float-delay-200": "float 3s ease-in-out infinite 0.2s",
+        "float-delay-600": "float 3s ease-in-out infinite 0.6s",
+      },
     },
   },
   plugins: [],
