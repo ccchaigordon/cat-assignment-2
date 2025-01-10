e92a60b9d75055c84ade27ff4954e5d7843ae5e7
Gordon Chai <chaigordon75@gmail.com>
2025-01-10 20:31:34 +0800

    Updated responsiveness

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -39,15 +39,15 @@ function About() {
             </div>
           </div>
         </div> */}
-        <div className="w-full bg-white py-28">
+        <div
+          ref={sectionRef}
+          className={`w-full bg-white py-28 ${
+            animate ? "fadeInUpTitle" : "opacity-0"
+          }`}
+        >
           <div className="flex flex-col lg:flex-row mx-12 xl:w-10/12 2xl:w-4/6 xl:mx-auto">
             {/* Text contents */}
-            <div
-              ref={sectionRef}
-              className={`flex flex-col w:full lg:w-1/2 ${
-                animate ? "fadeInUpTitle" : "opacity-0"
-              }`}
-            >
+            <div className="flex flex-col w:full lg:w-1/2">
               <div className="font-bold text-[#1c42ca] text-5xl sm:text-7xl text-center lg:text-justify">
                 Penang
               </div>
@@ -61,44 +61,44 @@ function About() {
               </div>
               <div className="flex flex-col gap-6 lg:gap-10 mt-12 lg:mt-16 mx-auto lg:mx-0">
                 <div className="flex flex-col sm:flex-row items-center justify-center sm:justify-start">
-                  <p className="text-[#1C42CA] text-2xl sm:text-4xl font-semibold text-center lg:text-right">
+                  <p className="text-[#1C42CA] text-2xl sm:text-4xl lg:text-3xl xl:text-4xl font-semibold text-center lg:text-left">
                     1.74 million
                   </p>
                   <div className="mt-1 sm:mt-0 sm:ml-4">
-                    <p className="text-md sm:text-xl text-center">
+                    <p className="text-md sm:text-xl lg:text-lg text-center">
                       people living in harmony
                     </p>
                   </div>
                 </div>
 
                 <div className="flex flex-col sm:flex-row items-center justify-center sm:justify-start">
-                  <p className="text-[#1C42CA] text-2xl sm:text-4xl font-semibold text-center lg:text-right">
+                  <p className="text-[#1C42CA] text-2xl sm:text-4xl lg:text-3xl xl:text-4xl font-semibold text-center lg:text-left">
                     200+
                   </p>
                   <div className="mt-1 sm:mt-0 sm:ml-4">
-                    <p className="text-md sm:text-xl text-center">
+                    <p className="text-md sm:text-xl lg:text-lg text-center">
                       years as a UNESCO World Heritage Site
                     </p>
                   </div>
                 </div>
 
                 <div className="flex flex-col sm:flex-row items-center justify-center sm:justify-start">
-                  <p className="text-[#1C42CA] text-2xl sm:text-4xl font-semibold text-left md:text-right">
+                  <p className="text-[#1C42CA] text-2xl sm:text-4xl lg:text-3xl xl:text-4xl font-semibold text-center lg:text-left">
                     100+
                   </p>
                   <div className="mt-1 sm:mt-0 sm:ml-4">
-                    <p className="text-md sm:text-xl text-center">
-                      street food earning the title of Food Heaven
+                    <p className="text-md sm:text-xl lg:text-lg text-center">
+                      street food with title of Food Heaven
                     </p>
                   </div>
                 </div>
 
                 <div className="flex flex-col sm:flex-row items-center justify-center sm:justify-start">
-                  <p className="text-[#1C42CA] text-2xl sm:text-4xl font-semibold text-left md:text-right">
+                  <p className="text-[#1C42CA] text-2xl sm:text-4xl lg:text-3xl xl:text-4xl font-semibold text-center lg:text-left">
                     70+
                   </p>
                   <div className="mt-1 sm:mt-0 sm:ml-4">
-                    <p className="text-md sm:text-xl text-center">
+                    <p className="text-md sm:text-xl lg:text-lg text-center">
                       tourist attractions
                     </p>
                   </div>
@@ -109,7 +109,7 @@ function About() {
             {/* Shape element */}
             <div
               ref={sectionRef}
-              className={`hidden sm:flex justify-center items-center w-full mt-4 lg:mt-0 xl:ml-[8vw] lg:w-[500px] ${
+              className={`hidden sm:flex justify-center items-center w-full mt-4 lg:mt-0 xl:ml-[8vw] lg:w-[550px] ${
                 animate ? "fadeInUpContent" : "opacity-0"
               }`}
             >
@@ -128,7 +128,7 @@ function About() {
                     className="w-full h-full object-cover rounded-full"
                   />
                 </div>
-                <div className="absolute flex justify-center items-center w-[30%] h-[30%] bottom-[10%] lg:bottom-[0%] right-[10%] lg:right-[0%] border-8 border-[white] rounded-full bg-[rgba(32,229,239,0.2)] overflow-hidden animate-float-delay-600">
+                <div className="absolute flex justify-center items-center w-[30%] h-[30%] bottom-[10%] lg:bottom-[0%] right-[10%] lg:right-[10%] border-8 border-[white] rounded-full bg-[rgba(32,229,239,0.2)] overflow-hidden animate-float-delay-600">
                   <img
                     src="/assets/mural_georgetown.jpg"
                     alt="Circle 2"
