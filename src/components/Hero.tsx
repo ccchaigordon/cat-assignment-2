fa50c23621f6e4af0ddb7d3098c3c35a9d4e6dbc
Gordon Chai <chaigordon75@gmail.com>
2025-01-11 12:19:22 +0800

    Updated layout

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,13 +4,13 @@ function Hero() {
   return (
     <div
       id="hero"
-      className="relative w-full h-screen overflow-hidden flex md:items-center"
+      className="relative w-full h-screen overflow-hidden flex items-center"
     >
       <video autoPlay muted className="absolute w-screen h-screen object-cover">
         <source src="/assets/hero_vid.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <div className="z-10 w-10/12 mx-auto mt-[30vh] md:mt-[5rem] text-center md:text-left text-white">
+      <div className="z-10 w-10/12 mx-auto md:mt-[5rem] text-center md:text-left text-white">
         <h1 className="hero-slide-left text-3xl md:text-7xl font-bold">
           Discover Penang
         </h1>
@@ -20,11 +20,24 @@ function Hero() {
           food, and immerse yourself in the vibrant local life. Begin your
           journey with us!
         </p>
-        <Link to="about" spy={true} smooth={true} offset={-50} duration={300}>
-          <button className="hero-slide-up-delay mt-10 bg-green-600 hover:bg-green-700 text-white font-semibold py-4 px-8 rounded-xl shadow-md transition duration-300 ease-in-out">
-            Discover More
-          </button>
-        </Link>
+        <div className="flex flex-col md:flex-row">
+          <Link to="about" spy={true} smooth={true} offset={-50} duration={300}>
+            <button className="hero-slide-up-delay mt-10 bg-green-600 hover:bg-green-700 text-white font-semibold py-4 px-8 rounded-xl shadow-md transition duration-300 ease-in-out">
+              Discover More
+            </button>
+          </Link>
+          <Link
+            to="gallery"
+            spy={true}
+            smooth={true}
+            offset={-50}
+            duration={300}
+          >
+            <button className="hero-slide-up-delay bg-transparent md:ml-4 text-white font-semibold items-center mt-4 md:mt-10 hover:bg-blue-700 py-4 px-8 rounded-xl shadow-md transition duration-300 ease-in-out">
+              Gallery
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
